545a3a0fb44f7f52c7a2789d815effc14059cf1c
Ajihadre <caron-theo@laposte.net>
2023-08-17 08:34:37 +0200

    First modification

---
+++ src/modelling/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 00:29:40 2023
+
+@author: Théo
+"""
+
---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 00:30:08 2023
+
+@author: Théo
+"""
+
